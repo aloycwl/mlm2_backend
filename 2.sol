d52dbba6f1635ea1cc903a620fe74404d6d6b4bf
Aloysius Chan <63489563+aloycwl@users.noreply.github.com>
2022-09-12 21:12:19 +0800

    Update 2.sol

--- 2.sol
+++ 2.sol
@@ -1,26 +1,46 @@
 /***
 Purchase: check upline eligible to matching
 Purchase: issue tokens to upline?
-Check withdrawal amount
-Change withdraw to individual
-Change to 180/360 days
+Withdrawal: stacking give to upline too?
 ***/
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
-interface IERC721{event Transfer(address indexed from,address indexed to,uint indexed tokenId);event Approval(address indexed owner,address indexed approved,uint indexed tokenId);event ApprovalForAll(address indexed owner,address indexed operator,bool approved);function balanceOf(address)external view returns(uint);function ownerOf(uint)external view returns(address);function safeTransferFrom(address,address,uint)external;function transferFrom(address,address,uint)external;function approve(address,uint)external;function getApproved(uint)external view returns(address);function setApprovalForAll(address,bool)external;function isApprovedForAll(address,address)external view returns(bool);function safeTransferFrom(address,address,uint,bytes calldata)external;}
-interface IERC721Metadata{function name()external view returns(string calldata);function symbol()external view returns(string calldata);function tokenURI(uint)external view returns(string calldata);}
-interface IERC20{function transferFrom(address,address,uint)external;function balanceOf(address)external view returns(uint256);}
-interface ISWAP{function getAmountsOut(uint,address,address)external view returns(uint);}
+interface IERC721{
+    event Transfer(address indexed from,address indexed to,uint indexed tokenId);
+    event Approval(address indexed owner,address indexed approved,uint indexed tokenId);
+    event ApprovalForAll(address indexed owner,address indexed operator,bool approved);
+    function balanceOf(address)external view returns(uint);
+    function ownerOf(uint)external view returns(address);
+    function safeTransferFrom(address,address,uint)external;
+    function transferFrom(address,address,uint)external;
+    function approve(address,uint)external;
+    function getApproved(uint)external view returns(address);
+    function setApprovalForAll(address,bool)external;
+    function isApprovedForAll(address,address)external view returns(bool);
+    function safeTransferFrom(address,address,uint,bytes calldata)external;
+}
+interface IERC721Metadata{
+    function name()external view returns(string calldata);
+    function symbol()external view returns(string calldata);
+    function tokenURI(uint)external view returns(string calldata);
+}
+interface IERC20{
+    function transferFrom(address,address,uint)external;
+    function balanceOf(address)external view returns(uint256);
+}
+interface ISWAP{
+    function getAmountsOut(uint,address,address)external view returns(uint);
+}
 contract ERC721AC_93N is IERC721,IERC721Metadata{
     struct User{
         address upline;
-        address[]downline;
+        address[]downline;  
         uint[]pack;
     }
     struct Pack{
         uint node;
         uint t93n;
         uint claimed;
-        uint joined;
+        uint minted;
         address owner;
     }
     struct Node{
@@ -38,7 +58,7 @@ contract ERC721AC_93N is IERC721,IERC721Metadata{
     mapping(address=>mapping(address=>bool))private _operatorApprovals;
     uint constant private P=10000; //Percentage
     uint[3]private refA=[500,300,200];
-    uint[3]private refB=[500,500,1000];
+    uint[3]private refB=[500,500,1e3];
     uint public Share;
     uint private _count; //For unique NFT
 
@@ -50,25 +70,56 @@ contract ERC721AC_93N is IERC721,IERC721Metadata{
         (_A[0]=user[msg.sender].upline=msg.sender,_A[1]=USDT,_A[2]=T93N,_A[3]=Swap,_A[4]=Tech);
         user[_A[0]].pack.push(0);
         user[_A[4]].pack.push(0);
-        (node[0].count,node[0].price,node[0].factor,node[0].uri)=(25e4,node[1].price=node[2].price=1e20,1,"bAXSCgPa1KkU9AABScYju6VxVy8F9NdPfUJxM3NsMWQt");
+        (node[0].count,node[0].price,node[0].factor,node[0].uri)=
+            (25e4,node[1].price=node[2].price=1e20,1,"bAXSCgPa1KkU9AABScYju6VxVy8F9NdPfUJxM3NsMWQt");
         (node[1].count,node[1].factor,node[1].uri)=(15e4,2,"XC9ZBbRaKSVqx6bqvpBtCRgySWju2hnbT5x9sRZhheZw");
         (node[2].count,node[2].factor,node[2].uri)=(1e5,3,"Z1vRU2Yf6BfZCdpTVRPzXUtoxAsxtPVjFk9aK2JxTtP2");
-        (node[3].count,node[3].price,node[3].period,node[3].factor,node[3].uri)=(3e4,1e21,180,1,"cUpTRu4AehAoGLGcYCEaCz9hR6bdB8shVmnmk5nNenyy");
-        (node[4].count,node[4].price,node[4].period,node[4].factor,node[4].uri)=(1e4,5e21,360,7,"bLKzHK2fCe4T8mdZ3NMk9yY4JwwNgS8gJeCfCEUmpkh7");
+        (node[3].count,node[3].price,node[3].period,node[3].factor,node[3].uri)=
+            (3e4,1e21,15552e3,1,"cUpTRu4AehAoGLGcYCEaCz9hR6bdB8shVmnmk5nNenyy");
+        (node[4].count,node[4].price,node[4].period,node[4].factor,node[4].uri)=
+            (1e4,5e21,31104e3,7,"bLKzHK2fCe4T8mdZ3NMk9yY4JwwNgS8gJeCfCEUmpkh7");
     }
-    function supportsInterface(bytes4 a)external pure returns(bool){return a==type(IERC721).interfaceId||a==type(IERC721Metadata).interfaceId;}
-    function approve(address a,uint b)external override{require(msg.sender==ownerOf(b)||isApprovedForAll(ownerOf(b),msg.sender));_tokenApprovals[b]=a;emit Approval(ownerOf(b),a,b);}
-    function getApproved(uint a)public view override returns(address){return _tokenApprovals[a];}
-    function setApprovalForAll(address a,bool b)external override{_operatorApprovals[msg.sender][a]=b;emit ApprovalForAll(msg.sender,a,b);}
-    function isApprovedForAll(address a,address b)public view override returns(bool){return _operatorApprovals[a][b];}
-    function ownerOf(uint a)public view override returns(address){return pack[a].owner;}
-    function owner()external view returns(address){return _A[0];}
-    function name()external pure override returns(string memory){return"Ninety Three N";}
-    function symbol()external pure override returns(string memory){return"93N";}
-    function balanceOf(address a)external view override returns(uint){return user[a].pack.length;}
-    function tokenURI(uint a)external view override returns(string memory){return string(abi.encodePacked("ipfs://Qm",node[a].uri));}
-    function safeTransferFrom(address a,address b,uint c)external override{transferFrom(a,b,c);}
-    function safeTransferFrom(address a,address b,uint c,bytes calldata)external override{transferFrom(a,b,c);}
+    function supportsInterface(bytes4 a)external pure returns(bool){
+        return a==type(IERC721).interfaceId||a==type(IERC721Metadata).interfaceId;
+    }
+    function approve(address a,uint b)external override{
+        require(msg.sender==ownerOf(b)||isApprovedForAll(ownerOf(b),msg.sender));
+        _tokenApprovals[b]=a;emit Approval(ownerOf(b),a,b);
+    }
+    function getApproved(uint a)public view override returns(address){
+        return _tokenApprovals[a];
+    }
+    function setApprovalForAll(address a,bool b)external override{
+        _operatorApprovals[msg.sender][a]=b;
+        emit ApprovalForAll(msg.sender,a,b);
+    }
+    function isApprovedForAll(address a,address b)public view override returns(bool){
+        return _operatorApprovals[a][b];
+    }
+    function ownerOf(uint a)public view override returns(address){
+        return pack[a].owner;
+    }
+    function owner()external view returns(address){
+        return _A[0];
+    }
+    function name()external pure override returns(string memory){
+        return"Ninety Three N";
+    }
+    function symbol()external pure override returns(string memory){
+        return"93N";
+    }
+    function balanceOf(address a)external view override returns(uint){
+        return user[a].pack.length;
+    }
+    function tokenURI(uint a)external view override returns(string memory){
+        return string(abi.encodePacked("ipfs://Qm",node[a].uri));
+    }
+    function safeTransferFrom(address a,address b,uint c)external override{
+        transferFrom(a,b,c);
+    }
+    function safeTransferFrom(address a,address b,uint c,bytes calldata)external override{
+        transferFrom(a,b,c);
+    }
     function transferFrom(address a,address b,uint c)public override{unchecked{
         /*
         Entire user will be duplicated to the new user
@@ -127,13 +178,41 @@ contract ERC721AC_93N is IERC721,IERC721Metadata{
         for(uint i;i<user[a].pack.length;i++)if(pack[user[a].pack[i]].node>2)return 1;
         return 0;
     }}
-    function getStaking()public view returns(uint){
+    function Withdraw()external{
         /*
         Calculate how much tbe sender should be getting
-        Loop through all existing nodes and check the expiry
+        Loop through all existing nodes and calculate since last claimed
+        Get the expiry and issue percentage when expired
         */
-
+        uint x;
+        uint[]memory p=user[msg.sender].pack;
+        for(uint i;i<p.length;i++){
+            uint z;
+            if(pack[user[msg.sender].pack[i]].node>2){
+                uint expiry=pack[p[i]].minted+node[p[i]].period;
+                if(expiry<block.timestamp)
+                    x+=pack[p[i]].t93n*node[p[i]].factor/P*(block.timestamp-pack[p[i]].claimed)/86400;
+                else{
+                    if(expiry+2628e3>block.timestamp&&expiry+2628e3>pack[p[i]].claimed)x+=pack[p[i]].t93n*2/5;
+                    if(expiry+5256e3>block.timestamp&&expiry+5256e3>pack[p[i]].claimed)x+=pack[p[i]].t93n*3/10;
+                    if(expiry+7884e3>block.timestamp&&expiry+5256e3>pack[p[i]].claimed){
+                        x+=pack[p[i]].t93n*3/10;
+                        popPackages(msg.sender,p[i]);
+                        emit Transfer(msg.sender,address(0),p[i]);
+                        z=1;
+                    }
+                }
+            }
+            if(z<1)pack[p[i]].claimed=block.timestamp;
+        }
+        IERC20(_A[2]).transferFrom(address(this),msg.sender,x);
     }
+    function getNodes()external view returns(uint[]memory n){unchecked{
+        /*
+        Return the current user nodes for selection to merge
+        */
+        n=user[msg.sender].pack;
+    }}
     function Purchase(address referral,uint n,uint c)external{unchecked{
         require((n<3?node[0].count+node[1].count+node[2].count:node[n].count)>=c,"Insufficient nodes");
         /*
@@ -170,9 +249,8 @@ contract ERC721AC_93N is IERC721,IERC721Metadata{
             }else num=n;
             mintNFT(num);
             Pack storage p=pack[_count];
-            (p.node,p.owner,p.t93n,p.joined)=(num,msg.sender,t93n,p.claimed=block.timestamp);
+            (p.node,p.owner,p.t93n,p.minted)=(num,msg.sender,t93n,p.claimed=block.timestamp);
             user[msg.sender].pack.push(_count);
-            
         }
     }}
     function Merging(uint[]calldata nfts)external{
